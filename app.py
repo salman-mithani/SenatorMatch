0ac83ba2e42cc104c8f6b1a2f137cf1221d23a6d
salman0907 <salman0907@gmail.com>
2017-03-07 17:41:30 -0600

    Added loading page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import json
 import requests
+import os
+import datetime
 
 class Senator:
     """docstring for Senator."""
@@ -55,8 +57,7 @@ def findVote(votes, idValue):
             return vote["vote_position"]
 
 
-def returnSenators(state):
-    senators = getSenators()
+def returnSenators(state, senators):
     idValues = []
     names = []
     findSenator(state, senators, idValues)
@@ -64,8 +65,7 @@ def returnSenators(state):
     names.append(senators[idValues[1]].name + " " + "( " + senators[idValues[1]].party + ")")
     return names
 
-def returnVotes(state, rollCall):
-    senators = getSenators()
+def returnVotes(state, rollCall, senators):
     idValues = []
     findSenator(state, senators, idValues)
     data = getVotes(rollCall)
@@ -98,6 +98,11 @@ def imageSearch(name):
 app = Flask(__name__)
 
 state = 'blank'
+senators = ''
+senator1 = ''
+senator2 = ''
+q1 = q2 = q3 = q4 = q5 = q6 = q7 = q8 = q9 = q10 = q11 = q12 = q13 = q14 = q15 = ''
+q1s1 = q1s2 = q2s1 = q2s2 = q3s1 = q3s2 = q4s1 = q4s2 = q5s1 = q5s2 = q6s1 = q6s2 = q7s1 = q7s2 = q8s1 = q8s2 = q9s1 = q9s2 = q10s1 = q10s2 = q11s1 = q11s2 = q12s1 = q12s2 = q13s1 = q13s3 = q14s1 = q14s2 = q15s1 = q15s2 = ''
 
 @app.route('/', methods=['GET','POST'])
 def homepage():
@@ -114,100 +119,114 @@ def quiz():
 		state = request.form['state']
 		return render_template('quiz.html', state = state)
 
-@app.route('/results', methods = ['GET','POST'])
-def results():
+@app.route('/loading', methods = ['GET','POST'])
+def loading():
 	if request.method == 'POST':
 		global state
 		state = str(state)
 
+		global senator1, senator2
+		global q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15
+		global q1s1, q1s2, q2s1, q2s2, q3s1, q3s2, q4s1, q4s2, q5s1, q5s2, q6s1, q6s2, q7s1, q7s2, q8s1, q8s2, q9s1, q9s2, q10s1, q10s2, q11s1, q11s2, q12s1, q12s2, q13s1, q13s2, q14s1, q14s2, q15s1, q15s2
 		global returnSenators
 		global returnVotes
-		names = returnSenators(state)
-		s1name = names[0]
-		s2name = names[1]
+		global getSenators
+		global senators
+		senators = getSenators()
 
 		q1 = request.form['q1']
-		q1votes = returnVotes(state, 334)
+		q1votes = returnVotes(state, 334, senators)
 		q1s1 = q1votes[0]
 		q1s2 = q1votes[1]
 
 		q2 = request.form['q2']
-		q2votes = returnVotes(state, 331)
+		q2votes = returnVotes(state, 331, senators)
 		q2s1 = q2votes[0]
 		q2s2 = q2votes[1]
 
 		q3 = request.form['q3']
-		q3votes = returnVotes(state, 329)
+		q3votes = returnVotes(state, 329, senators)
 		q3s1 = q3votes[0]
 		q3s2 = q3votes[1]
 
 		q4 = request.form['q4']
-		q4votes = returnVotes(state, 325)
+		q4votes = returnVotes(state, 325, senators)
 		q4s1 = q4votes[0]
 		q4s2 = q4votes[1]
 
 		q5 = request.form['q5']
-		q5votes = returnVotes(state, 291)
+		q5votes = returnVotes(state, 291, senators)
 		q5s1 = q5votes[0]
 		q5s2 = q5votes[1]
 
 		q6 = request.form['q6']
-		q6votes = returnVotes(state, 239)
+		q6votes = returnVotes(state, 239, senators)
 		q6s1 = q6votes[0]
 		q6s2 = q6votes[1]
 
 		q7 = request.form['q7']
-		q7votes = returnVotes(state, 238)
+		q7votes = returnVotes(state, 238, senators)
 		q7s1 = q7votes[0]
 		q7s2 = q7votes[1]
 
 		q8 = request.form['q8']
-		q8votes = returnVotes(state, 233)
+		q8votes = returnVotes(state, 233, senators)
 		q8s1 = q8votes[0]
 		q8s2 = q8votes[1]
 
 		q9 = request.form['q9']
-		q9votes = returnVotes(state, 189)
+		q9votes = returnVotes(state, 189, senators)
 		q9s1 = q9votes[0]
 		q9s2 = q9votes[1]
 
 		q10 = request.form['q10']
-		q10votes = returnVotes(state, 132)
+		q10votes = returnVotes(state, 132, senators)
 		q10s1 = q10votes[0]
 		q10s2 = q10votes[1]
 
 		q11 = request.form['q11']
-		q11votes = returnVotes(state, 131)
+		q11votes = returnVotes(state, 131, senators)
 		q11s1 = q11votes[0]
 		q11s2 = q11votes[1]
 
 		q12 = request.form['q12']
-		q12votes = returnVotes(state, 111)
+		q12votes = returnVotes(state, 111, senators)
 		q12s1 = q12votes[0]
 		q12s2 = q12votes[1]
 
 		q13 = request.form['q13']
-		q13votes = returnVotes(state, 78)
+		q13votes = returnVotes(state, 78, senators)
 		q13s1 = q13votes[0]
 		q13s2 = q13votes[1]
 
 		q14 = request.form['q14']
-		q14votes = returnVotes(state, 44)
+		q14votes = returnVotes(state, 44, senators)
 		q14s1 = q14votes[0]
 		q14s2 = q14votes[1]
 
 		q15 = request.form['q15']
-		q15votes = returnVotes(state, 31)
+		q15votes = returnVotes(state, 31, senators)
 		q15s1 = q15votes[0]
 		q15s2 = q15votes[1]
 
-		userVotes = {334:q1, 331:q2, 329: q3, 325:q4, 291:q5, 239:q6, 238:q7, 233:q8, 189:q9, 132:q10, 131:q11, 111:q12, 78:q13, 44:q14, 31:q15}
+		return render_template('loading.html')
 
-		global getSenators
+@app.route('/results', methods = ['GET','POST'])
+def results():
+	if request.method == 'POST':
+		global senator1, senator2
+		global q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15
+		global q1s1, q1s2, q2s1, q2s2, q3s1, q3s2, q4s1, q4s2, q5s1, q5s2, q6s1, q6s2, q7s1, q7s2, q8s1, q8s2, q9s1, q9s2, q10s1, q10s2, q11s1, q11s2, q12s1, q12s2, q13s1, q13s2, q14s1, q14s2, q15s1, q15s2
 		global maxSenators
 		global imageSearch
 		global count
-		senators = getSenators()
+
+		userVotes = {334:q1, 331:q2, 329: q3, 325:q4, 291:q5, 239:q6, 238:q7, 233:q8, 189:q9, 132:q10, 131:q11, 111:q12, 78:q13, 44:q14, 31:q15}
+
+		names = returnSenators(state, senators)
+		s1name = names[0]
+		s2name = names[1]
+
 		person = User(senators, userVotes)
 		count(person)
 
@@ -218,4 +237,5 @@ def results():
 		return render_template('results.html', senator1 = s1name, senator2 = s2name, q1s1 = q1s1, q1s2 = q1s2, q2s1 = q2s1, q2s2 = q2s2, q3s1 = q3s1, q3s2 = q3s2, q4s1 = q4s1, q4s2 = q4s2, q5s1 = q5s1, q5s2 = q5s2, q6s1 = q6s1, q6s2 = q6s2, q7s1 = q7s1, q7s2 = q7s2, q8s1 = q8s1, q8s2 = q8s2, q9s1 = q9s1, q9s2 = q9s2, q10s1 = q10s1, q10s2 = q10s2, q11s1 = q11s1, q11s2 = q11s2, q12s1 = q12s1, q12s2 = q12s2, q13s1 = q13s1, q13s2 = q13s2, q14s1 = q14s1, q14s2 = q14s2, q15s1 = q15s1, q15s2 = q15s2, q1 = q1, q2 = q2, q3 = q3, q4 = q4, q5 = q5, q6 = q6, q7 = q7, q8 = q8, q9 = q9, q10 = q10, q11 = q11, q12 = q13, q13 = q13, q14 = q14, q15 = q15, output = output, filename = imglink)
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
